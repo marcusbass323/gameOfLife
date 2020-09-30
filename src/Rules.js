2337ce47c1aca92485fe06747833a195afcd8215
Marcus Bass <mbass@Marcuss-MacBook-Pro.local>
2020-09-30 16:43:37 -0700

    counting generations

--- src/Rules.js
+++ src/Rules.js
@@ -7,10 +7,10 @@ export default function Rules() {
             <About />
             <h1>Rules:</h1>
         <div id="Rules">
-                <div className="rule"><h1 className="ruleNumber">1</h1><div>Any live cell with fewer than two live neighbours dies, as if by underpopulation.</div></div>
-                <div className="rule"><div><h1 className="ruleNumber">2</h1>Any live cell with two or three live neighbours lives on to the next generation.</div></div>
-                <div className="rule"><div><h1 className="ruleNumber">3</h1>Any live cell with more than three live neighbours dies, as if by overpopulation.</div></div>
-                <div className="rule"><div><h1 className="ruleNumber">4</h1>Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</div></div>
+                <div className="rule"><h1 className="ruleNumber">1</h1><div>Any live cell with fewer than two live neighbors dies, as if by underpopulation.</div></div>
+                <div className="rule"><div><h1 className="ruleNumber">2</h1>Any live cell with two or three live neighbors lives on to the next generation.</div></div>
+                <div className="rule"><div><h1 className="ruleNumber">3</h1>Any live cell with more than three live neighbors dies, as if by overpopulation.</div></div>
+                <div className="rule"><div><h1 className="ruleNumber">4</h1>Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.</div></div>
         </div>
         </div>
 
